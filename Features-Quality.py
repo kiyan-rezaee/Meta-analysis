c239526342e0d134a576b59d19d565c342c43493
Mehrshad Dadashzadeh <mehrdadashzadeh1379@gmail.com>
2022-11-23 20:46:55 +0330

    updated

--- Features-Quality.py
+++ Features-Quality.py
@@ -4,15 +4,13 @@ import PyPDF2
 import pyperclip
 
 # set your path here and change the name below
-kiyan=r"C:\Users\kiyan\Desktop\research code\code\papers\j.knosys.2015.11.002.pdf"
+kiyan = r"C:\Users\kiyan\Desktop\research code\code\papers\j.knosys.2015.11.002.pdf"
 sara = r"C:\Users\saram\Desktop\10.pdf"
-morteza=r""
+morteza = r""
 mehrshad = r"C:\Documents\Mehrshad\User Intent Modeling\Collaborative filtering with social regularization for TV program recommendation.pdf"
 
 # creating a pdf file object
-pdfFileObj = open(
-    mehrshad,
-    'rb')
+pdfFileObj = open(mehrshad, 'rb')
 
 quality = {
     '["Performance"]': "",
