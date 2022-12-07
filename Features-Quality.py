6e1baf705906dea6c171804a98a32e68184de1af
saramazaheri <61461140+saramazaheri@users.noreply.github.com>
2022-12-07 22:43:13 +0330

    Update Features-Quality.py

--- Features-Quality.py
+++ Features-Quality.py
@@ -6,12 +6,12 @@ import pyperclip
 
 # set your path here and change the name below
 kiyan = r"C:\Users\kiyan\Desktop\research code\code\papers\978-3-642-30284-8_9.pdf"
-sara = r"C:\Users\saram\Desktop\9.pdf"
+sara = r"C:\Users\saram\Desktop\2.pdf"
 morteza = r"../ATaxonomyOfWebSearch.pdf"
 mehrshad = shortcut(613)
 
 # creating a pdf file object
-pdfFileObj = open(morteza, 'rb')
+pdfFileObj = open(sara, 'rb')
 
 quality = [
     ["Performance"],
