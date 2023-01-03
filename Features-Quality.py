93363c584bd7e8aa33e8703db47dc05c152e7523
Mehrshad Dadashzadeh <mehrdadashzadeh1379@gmail.com>
2023-01-03 14:21:35 +0330

    updated

--- Features-Quality.py
+++ Features-Quality.py
@@ -8,10 +8,10 @@ import pyperclip
 kiyan = r"C:\Users\kiyan\Desktop\research code\code\papers\1611.03971.pdf"
 sara = r"C:\Users\saram\Desktop\10.pdf"
 morteza = r"../Community_aware_user_profile_enrichment.pdf"
-mehrshad = shortcut(813)
+mehrshad = shortcut(815)
 
 # creating a pdf file object
-pdfFileObj = open(sara, 'rb')
+pdfFileObj = open(mehrshad, 'rb')
 
 quality = [
     ["Performance"],
@@ -264,6 +264,7 @@ features = [
     ["lemmatization", 'Lemmatisation'],
     ["word embedding"],
     ["opinion mining", "opinion-mining", "mine opinion"],
+    ["Session-based","Session based"],
     ["Age Detection"],
 ]
 print(f"Qualities count: {len(quality)}")
